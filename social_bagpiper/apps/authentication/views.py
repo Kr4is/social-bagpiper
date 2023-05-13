0212d6377216de64682ad39302f9a2d7eeb39c47
Pedrety <pedrety998@gmail.com>
2023-05-13 16:07:54 +0200

    Add errors to wrong forms

--- social_bagpiper/apps/authentication/views.py
+++ social_bagpiper/apps/authentication/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 
@@ -5,39 +6,45 @@ from . import forms
 
 
 def login_page(request):
-    message = ""
-    form = None
+    log_in_form = forms.LoginForm(
+        request.POST or None,
+    )
+    sign_in_form = forms.RegisterForm(request.POST or None, request.FILES or None)
     if request.method == "POST":
         if "log_in" in request.POST:
-            form = forms.LoginForm(request.POST)
-            if form.is_valid():
+            if log_in_form.is_valid():
                 user = authenticate(
-                    username=form.cleaned_data["username"],
-                    password=form.cleaned_data["password"],
+                    username=log_in_form.cleaned_data["username"],
+                    password=log_in_form.cleaned_data["password"],
                 )
                 if user is not None:
                     login(request, user)
                     return redirect("home")
                 else:
-                    form.add_error(None, "Invalid username or password!")
+                    log_in_form.add_error(None, "Invalid username or password!")
 
         elif "sign_in_user" in request.POST:
-            form = forms.RegisterForm(request.POST)
-            if form.is_valid():
-                user = form.save()
+            if sign_in_form.is_valid():
+                user = sign_in_form.save()
                 user.refresh_from_db()
                 user.save()
-                raw_password = form.cleaned_data.get("password1")
+                raw_password = sign_in_form.cleaned_data.get("password1")
                 user = authenticate(username=user.username, password=raw_password)
                 login(request, user)
 
                 # redirect user to home page
                 return redirect("home")
             else:
-                message = "SignUp failed!"
+                messages.error(request, "Error")
 
     return render(
-        request, "authentication/login.html", context={"message": message, "form": form}
+        request,
+        "authentication/login.html",
+        context={
+            "message": messages,
+            "log_in_form": log_in_form,
+            "sign_in_form": sign_in_form,
+        },
     )
 
 
